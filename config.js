c01ae11190651b16350a86347de006aa335eda23
posh <101807149+ImPosh@users.noreply.github.com>
2023-10-17 20:20:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://worker-crimson-fire-799a.joshuavonharten.workers.dev",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODNlMmRjOTZiOTNjMTcwZjkyMmZjYWZlZTc2ZjA2YyIsInN1YiI6IjY1MmVjMjk2ZWE4NGM3MDBlYmEzN2IyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kv6CTsv8kkwdBkRTAu3LGWewVDoqN3jouBA1W2d3LPY"
 };
